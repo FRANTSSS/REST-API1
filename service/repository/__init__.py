301ca773469f76430ac4bd8522b776cb7c046181
FRANTSSS <90570390+FRANTSSS@users.noreply.github.com>
2022-06-27 16:14:14 +0600

    Delete service directory

--- service/repository/__init__.py
+++
@@ -1,9 +0,0 @@
-from .IUserRepository import IUserRepository
-from .PostgresUserRepository import PostgresUserRepository
-from .RedisUserRepository import RedisUserRepository
-
-__all__ = [
-    "IUserRepository",
-    "PostgresUserRepository",
-    "RedisUserRepository"
-]
